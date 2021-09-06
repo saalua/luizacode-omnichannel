0adb5dabe65f011e33ff8d7b09e83cfc01d0e75e
Julia Dutra <juliadutragoncalves@gmail.com>
2021-09-05 22:34:56 -0300

    Adicionei validação de parâmetros nos endpoints de recuperar e finalizar um pedido

--- src/api/pedidos.js
+++ src/api/pedidos.js
@@ -8,15 +8,26 @@ const { PedidoService, FINALIZAR_PEDIDO } = require('../services/pedidos')
 
 const pedidoService = new PedidoService(pedido)
 
-router.get('/:idPedido', async (req, res) => {
-    const { idPedido } = req.params
-    const pedidoEncontrado = await pedidoService.getById(idPedido)
-    if (pedidoEncontrado == null) {
-        res.status(404).send()
-    } else {
-        res.status(200).json(pedidoEncontrado)
-    }
-})
+router.get('/:idPedido',
+    check('idPedido')
+        .not().isEmpty()
+        .matches(/\d/)
+        .withMessage('Para consultar um pedido é necessário informar o seu id, que precisa ser um valor numérico'),
+    async (req, res) => {
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { idPedido } = req.params
+        const pedidoEncontrado = await pedidoService.getById(idPedido)
+        if (pedidoEncontrado == null) {
+            res.status(404).send()
+        } else {
+            res.status(200).json(pedidoEncontrado)
+        }
+    })
 
 router.get('/',
     check('idCliente')
@@ -34,20 +45,29 @@ router.get('/',
         res.status(200).json(pedidos)
     })
 
-router.post('/:idPedido/finalizar', async (req, res) => {
-    const { idPedido } = req.params
-    const resultado = await pedidoService.finalizarPedido(idPedido)
-    switch(resultado) {
-        case FINALIZAR_PEDIDO.FINALIZADO:
-            res.status(200).send()
-            break
-        case FINALIZAR_PEDIDO.PEDIDO_NAO_ENCONTRADO:
-            res.status(404).send()
-            break
-        case FINALIZAR_PEDIDO.STATUS_PEDIDO_IMPEDE_FINALIZAR:
-            res.status(400).json({ errors: [{msg: 'Só é possível finalizar pedidos que estejam em andamento'}]})
-            break
-    }
-})
+router.post('/:idPedido/finalizar',
+    check('idPedido')
+        .not().isEmpty()
+        .matches(/\d/)
+        .withMessage('Para finalizar um pedido é obrigatório informar o seu id, que precisa ser um valor numérico'),
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { idPedido } = req.params
+        const resultado = await pedidoService.finalizarPedido(idPedido)
+        switch (resultado) {
+            case FINALIZAR_PEDIDO.FINALIZADO:
+                res.status(200).send()
+                break
+            case FINALIZAR_PEDIDO.PEDIDO_NAO_ENCONTRADO:
+                res.status(404).send()
+                break
+            case FINALIZAR_PEDIDO.STATUS_PEDIDO_IMPEDE_FINALIZAR:
+                res.status(400).json({ errors: [{ msg: 'Só é possível finalizar pedidos que estejam em andamento' }] })
+                break
+        }
+    })
 
 module.exports = router
