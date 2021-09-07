a7aac49f7f12654938335f2efc7f790af3749f7e
Walquirialima <walquirialima2018@gmail.com>
2021-09-06 23:41:07 -0300

    endpoint clientes e produtos

--- src/api/produtos.js
+++ src/api/produtos.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router()
 
-const { produto } = require('../models')
+const { produto } = require('../models');
+const pedido = require('../models/pedido');
 const ProdutoService = require('../services/produtos')
+const {check, validationResult} = require('express-validator')
 
 const produtoService = new ProdutoService(produto)
 
@@ -11,4 +13,15 @@ router.get('/', async (req, res) => {
   res.status(200).json(produtos)
 })
 
+router.post('/pedidos/clientes', async (req, res) =>{
+  const {idCLiente, idPedidos, idProdutos} = req.body
+  try{
+    await pedido.create({idCLiente, idPedidos, idProdutos})
+    res.status(201).send('Cliente cadastrado com sucesso')
+  } catch(erro){
+    res.status(400).send('Não foi possivel cadastrar o cliente')
+  }
+});
+
 module.exports = router
+
