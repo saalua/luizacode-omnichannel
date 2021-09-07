91b7d0ba40c8785e55afe081ce1a3359c572f632
Walquirialima <walquirialima2018@gmail.com>
2021-09-07 00:52:28 -0300

    alteracao no endpoint adicionar novo produto

--- src/api/produtos.js
+++ src/api/produtos.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router()
 
 const { produto } = require('../models');
-const pedido = require('../models/pedido');
 const ProdutoService = require('../services/produtos')
-const {check, validationResult} = require('express-validator')
 
 const produtoService = new ProdutoService(produto)
 
@@ -12,16 +10,6 @@ router.get('/', async (req, res) => {
   const produtos = await produtoService.get()
   res.status(200).json(produtos)
 })
-
-router.post('/pedidos/clientes', async (req, res) =>{
-  const {idCLiente, idPedidos, idProdutos} = req.body
-  try{
-    await pedido.create({idCLiente, idPedidos, idProdutos})
-    res.status(201).send('Cliente cadastrado com sucesso')
-  } catch(erro){
-    res.status(400).send('Não foi possivel cadastrar o cliente')
-  }
-});
 
 module.exports = router
 
