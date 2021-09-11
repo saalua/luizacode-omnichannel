0bec3a32088772444eac8f29564a29b8385f7c10
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-09-11 11:31:17 -0300

    Fix - Mudanças no Banco

--- src/models/index.js
+++ src/models/index.js
@@ -13,20 +13,24 @@ const cliente = Cliente(sequelize, Sequelize.DataTypes);
 const Pedido = require('./pedido');
 const pedido = Pedido(sequelize, Sequelize.DataTypes);
 
+const ProdutosPedidos = require('./produtosPedido');
+const produtosPedido = ProdutosPedidos(sequelize, Sequelize.DataTypes);
+
 cliente.hasMany(pedido);
 pedido.belongsTo(cliente);
 
 loja.hasMany(pedido);
 pedido.belongsTo(loja);
 
-pedido.belongsToMany(produto, { through: 'ProdutosPedidos' });
-produto.hasMany(pedido);
+pedido.hasMany(produtosPedido);
+produto.hasMany(produtosPedido);
 
 const db = {
   loja,
   produto,
   cliente,
   pedido,
+  produtosPedido,
   sequelize,
 };
 
--- src/models/produtosPedido.js
+++ src/models/produtosPedido.js
@@ -2,19 +2,17 @@ module.exports = (sequelize, DataTypes) => {
     const ProdutosPedidos = sequelize.define(
         'ProdutosPedidos',
         {
-            idPedido: {
-                types: DataTypes.INTEGER,
-                allowNull: false
-            },
-            idProduto: {
-                types: DataTypes.INTEGER,
-                allowNull: false
+            valor: {
+                type: DataTypes.FLOAT,
+                allowNull: false,
             },
         },
         {
             timestamps: false,
         }
     );
+
+    // ProdutosPedidos.removeAttribute('id');
 
     return ProdutosPedidos;
 };
