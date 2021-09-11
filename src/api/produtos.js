710abdd52ef4912f273a2ceaee8114b015d9e9bf
Luana <saluana@outlook.com>
2021-09-10 21:53:15 -0300

    add validacao do id no endpoint de produtos/:id

--- src/api/produtos.js
+++ src/api/produtos.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { produto } = require('../models');
 const ProdutoService = require('../services/produtos');
+const { check, validationResult } = require('express-validator');
 
 const produtoService = new ProdutoService(produto);
 
@@ -11,10 +12,26 @@ router.get('/', async (req, res) => {
   res.status(200).json(produtos);
 });
 
-router.get('/:id', async (req, res) => {
-  const idProduto = req.params.id;
-  const produto = await produtoService.getProdutoById(idProduto);
-  res.status(200).json(produto);
+router.get('/:id',
+    check('id')
+        .not().isEmpty()
+        .matches(/\d/)
+        .withMessage('Para consultar um produto é preciso informar seu id, que precisa ser um valor numérico'),
+    async (req, res) => {
+        
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        
+        const idProduto = req.params.id;
+        const produto = await produtoService.getProdutoById(idProduto);
+        
+        if(produto == null) {
+            res.status(404).json("Produto não encontrado")
+        } else {
+            res.status(200).json(produto);
+        }
 });
 
 module.exports = router;
