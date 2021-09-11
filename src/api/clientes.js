953d957e403b31fe9879b9da83584fd0185593fb
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-09-11 20:49:33 -0300

    Feat - Autenticaçao usuario e cadastro validado

--- src/api/clientes.js
+++ src/api/clientes.js
@@ -1,28 +1,134 @@
+require('dotenv').config();
+
 const express = require('express');
+const md5 = require('md5');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
 const { cliente } = require('../models');
+const ClienteService = require('../services/clientes');
+const clienteService = new ClienteService(cliente);
 
-router.post('/', async (req, res) =>{
+const jwt = require('jsonwebtoken');
+const JWTSecret = process.env.JWTSecret;
 
-/*
-    #swagger.tags = ['Clientes']
-    #swagger.description = 'Endpoint para o cadastro do cliente.' 
-    #swagger.responses[200] = {
-    schema: { $ref: "#/definitions/Clientes"},
-    description: 'Cadastro realizado.'
+router.post(
+  '/',
+  body('nome')
+    .notEmpty()
+    .trim()
+    .escape()
+    .withMessage('O campo "nome" é obrigatótio'),
+  body('logradouro')
+    .notEmpty()
+    .trim()
+    .escape()
+    .withMessage('O campo "logradouro" é obrigatótio'),
+  body('bairro')
+    .notEmpty()
+    .trim()
+    .escape()
+    .withMessage('O campo "bairro" é obrigatótio'),
+  body('cidade')
+    .notEmpty()
+    .trim()
+    .escape()
+    .withMessage('O campo "cidade" é obrigatótio'),
+  body('cep')
+    .notEmpty()
+    .matches(/\d{8}/)
+    .withMessage('O campo "cep" deve conter 8 dígitos'),
+  body('email')
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('O campo "email" é obrigatótio'),
+  body('senha')
+    .notEmpty()
+    .trim()
+    .escape()
+    .isLength({ min: 6 })
+    .withMessage('O campo "senha" deve conter ao menos 6 caracteres'),
+  async (req, res) => {
+    /*
+			#swagger.tags = ['Clientes']
+			#swagger.description = 'Endpoint para o cadastro do cliente.' 
+			#swagger.responses[200] = {
+			schema: { $ref: "#/definitions/Clientes"},
+			description: 'Cadastro realizado.'
+			}
+			#swagger.responses[400] = {
+			description: 'Houve algum erro na requisição.'
+			}
+		*/
+    const erros = validationResult(req);
+
+    if (!erros.isEmpty()) {
+      return res.status(400).json({ erros: erros.array() });
     }
-    #swagger.responses[400] = {
-    description: 'Houve algum erro na requisição.'
-    }
-*/
 
-  const {nome, endereco, bairro, cidade, cep} = req.body
-  try{
-    await cliente.create({nome, endereco, bairro, cidade, cep})
-    res.status(201).send('Cliente cadastrado com sucesso')
-  } catch(erro){
-    res.status(400).send('Não foi possivel cadastrar o cliente')
+    const { nome, logradouro, bairro, cidade, cep, email, senha } = req.body;
+
+    try {
+      await clienteService.createUser(
+        nome,
+        logradouro,
+        bairro,
+        cidade,
+        cep,
+        email,
+        md5(senha)
+      );
+      res.status(201).send('Cliente cadastrado com sucesso');
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
   }
-});
+);
 
-module.exports = router
+router.post(
+  '/login',
+  body('email')
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('O campo "email" é obrigatótio'),
+  body('senha')
+    .notEmpty()
+    .trim()
+    .escape()
+    .isLength({ min: 6 })
+    .withMessage('O campo "senha" deve conter ao menos 6 caracteres'),
+  async (req, res) => {
+    const erros = validationResult(req);
+
+    if (!erros.isEmpty()) {
+      return res.status(400).json({ erros: erros.array() });
+    }
+
+    const { email, senha } = req.body;
+
+    const findCliente = await clienteService.getByEmail(email);
+
+    if (findCliente) {
+      if (findCliente.senha == md5(senha)) {
+        jwt.sign(
+          { id: findCliente.id, email: findCliente.email },
+          JWTSecret,
+          { expiresIn: '48h' },
+          (err, token) => {
+            if (err) {
+              res.status(400).json('Houve alguma falha interna!');
+            } else {
+              res.status(200).json({ token: token });
+            }
+          }
+        );
+      } else {
+        res.status(401).send('Credenciais invalidas!');
+      }
+    } else {
+      res.status(404).send('Email nao cadastrado no banco!');
+    }
+  }
+);
+
+module.exports = router;
---
+++ src/api/validateAuth.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const getTokenData = require('./functions');
+
+const JWTSecret = process.env.JWTSecret;
+
+module.exports = (req, res, next) => {
+  const authToken = req.headers['authorization'];
+  const bearer = authToken.split(' ');
+  const token = bearer[1];
+
+  if (token) {
+    if (getTokenData(token)) {
+      req.token = token;
+      next();
+    } else {
+      res.status(401).json('Token invalido!');
+    }
+  } else {
+    res.status(401).send('Token não encontrado!');
+  }
+};
