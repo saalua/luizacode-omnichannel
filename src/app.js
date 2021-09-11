46e5c04052a1ed19d8723a402b72af542c5696f0
Luana <saluana@outlook.com>
2021-09-11 18:57:09 -0300

    add cors config

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const routers = require('./api')
-const { sequelize } = require('./models')
+const express = require('express');
+const routers = require('./api');
+const { sequelize } = require('./models');
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
 
-const app = express()
+const cors = require('cors');
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 app.use(express.urlencoded({  extended:true }));
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use('/', routers)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/', routers);
+
+app.use(cors({
+    origin: '*'
+}));
 
 sequelize.sync().then(() => {
     console.log('Conectado com o banco com sucesso!')
-})
+});
 
 var server = app.listen(process.env.PORT || 3000, () => {
     console.log("Server listening on port %d in %s mode", server.address().port, app.settings.env);
